66640cf9e0d0a30acc86be92a775120727628a28
Marcus <marcus@lastcraft.com>
2013-06-10 21:12:56 +0100

    Fixing printf type

--- type.h
+++ type.h
@@ -5,12 +5,12 @@
 
 typedef struct Object_ Object;
 typedef struct Type_ Type;
-typedef void (*Printf)(char *, ...);
+typedef int (*Printf)(char *, ...);
 
 Type *declare(void (*destructor)(void *), void (*writer)(void *, Printf));
 Object *wrap(Type *, void *);
 void destroy(Object *);
 void *value(Object *);
-void write_object(Object *, void (*printer)(char *, ...));
+void write_object(Object *, Printf);
 
 #endif
