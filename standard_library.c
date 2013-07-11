9d913fb41081b83578468f4c5fc5db0110a46e6a
Marcus <marcus@lastcraft.com>
2013-07-11 20:38:39 +0100

    Parsing defun with ugly code

--- standard_library.c
+++ standard_library.c
@@ -14,9 +14,11 @@ static Object *set(Object *, ErrorHandler, Binding *);
 static Object *set_pling(Object *, ErrorHandler, Binding *);
 static Object *setq(Object *, ErrorHandler, Binding *);
 static Object *setq_pling(Object *, ErrorHandler, Binding *);
+static Object *defun(Object *, ErrorHandler, Binding *);
+static Object *plus(Object *, ErrorHandler, Binding *);
 static Object *set_value(Object *, Object *, ErrorHandler, Binding *);
 static Object *overwrite_value(Object *, Object *, Binding *);
-static Object *plus(Object *, ErrorHandler, Binding *);
+static int is_argument_list(Object *);
 
 void declare_standard_library(Binding *binding) {
     add(binding, "quit", built_in(quit));
@@ -26,6 +28,7 @@ void declare_standard_library(Binding *binding) {
     add(binding, "set!", built_in(set_pling));
     add(binding, "setq", special_form(built_in(setq)));
     add(binding, "setq!", special_form(built_in(setq_pling)));
+    add(binding, "defun", special_form(built_in(defun)));
     add(binding, "+", built_in(plus));
 }
 
@@ -69,6 +72,47 @@ static Object *setq_pling(Object *arguments, ErrorHandler error, Binding *bindin
     return overwrite_value(symbol, rvalue, binding);
 }
 
+static Object *defun(Object *arguments, ErrorHandler error, Binding *binding) {
+    Object *symbol = clone(car(arguments));
+    if (! is_identifier(symbol)) {
+        destroy(arguments);
+        return error("Not an identifier", symbol);
+    }
+    Object *parameters = clone(car(cdr(arguments)));
+    if (! is_argument_list(parameters)) {
+        destroy(symbol);
+        destroy(arguments);
+        return error("Not an argument list", parameters);
+    }
+    Object *block = clone(car(cdr(cdr(arguments))));
+    if (! is_pair(block)) {
+        destroy(symbol);
+        destroy(parameters);
+        destroy(arguments);
+        return error("Not a code block", block);
+    }
+    Object *new_lambda = lambda(parameters, block);
+    return set_value(symbol, new_lambda, error, binding);
+}
+
+static Object *plus(Object *arguments, ErrorHandler error, Binding *binding) {
+    long total = 0L;
+    Object *tail;
+    while (! is_nil(arguments)) {
+        tail = clone(cdr(arguments));
+        if (! is_number(car(arguments))) {
+            Object *bad_number = clone(car(arguments));
+            destroy(arguments);
+            destroy(tail);
+            return error("Not a number", (void *)bad_number);
+        }
+        total = total + *(long *)value(car(arguments));
+        destroy(arguments);
+        arguments = tail;
+    }
+    return number(total);
+}
+
 static Object *set_value(Object *symbol, Object *rvalue, ErrorHandler error, Binding *binding) {
     if (! is_identifier(symbol)) {
         destroy(rvalue);
@@ -90,20 +134,6 @@ static Object *overwrite_value(Object *symbol, Object *rvalue, Binding *binding)
     return nil();
 }
 
-static Object *plus(Object *arguments, ErrorHandler error, Binding *binding) {
-    long total = 0L;
-    Object *tail;
-    while (! is_nil(arguments)) {
-        tail = clone(cdr(arguments));
-        if (! is_number(car(arguments))) {
-            Object *bad_number = clone(car(arguments));
-            destroy(arguments);
-            destroy(tail);
-            error("Not a number", (void *)bad_number);
-        }
-        total = total + *(long *)value(car(arguments));
-        destroy(arguments);
-        arguments = tail;
-    }
-    return number(total);
+static int is_argument_list(Object *list) {
+    return 1;
 }
