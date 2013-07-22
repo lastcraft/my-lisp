813bbd90f42774c3490f940ee10cf32729f2aff5
Marcus <marcus@lastcraft.com>
2013-07-22 10:51:53 +0100

    Fixed segfault on missing false block

--- standard_library.c
+++ standard_library.c
@@ -106,7 +106,8 @@ static Object *defun(Object *arguments, ErrorHandler error, Binding *binding) {
 static Object *branch(Object *arguments, ErrorHandler error, Binding *binding) {
     bool condition;
     Try {
-        condition = is_true(eval(clone(car(arguments)), error, binding));
+        Object *argument_result = eval(clone(car(arguments)), error, binding);
+        condition = is_true(argument_result);
     } Catch {
         destroy(arguments);
         return rethrow();
@@ -114,27 +115,18 @@ static Object *branch(Object *arguments, ErrorHandler error, Binding *binding) {
     Object *true_block = clone(car(cdr(arguments)));
     if (is_nil(true_block)) {
         destroy(arguments);
-        return error("No block to execute", nil());
+        return error("No block to execute", true_block);
+    } else if (condition) {
+        destroy(arguments);
+        return eval(true_block, error, binding);
     }
+    destroy(true_block);
     Object *false_block = clone(car(cdr(cdr(arguments))));
     destroy(arguments);
-    Object *result;
-    Try {
-        if (condition) {
-            result = eval(true_block, error, binding);
-            destroy(false_block);
-        } else if (! is_nil(false_block)) {
-            result = eval(false_block, error, binding);
-            destroy(true_block);
-        } else {
-            destroy(true_block);
-            destroy(false_block);
-        }
-    } Catch {
-        destroy(condition ? false_block : true_block);
-        return rethrow();
+    if (! is_nil(false_block)) {
+        return eval(false_block, error, binding);
     }
-    return result;
+    return nil();
 }
 
 static Object *plus(Object *arguments, ErrorHandler error, Binding *binding) {
